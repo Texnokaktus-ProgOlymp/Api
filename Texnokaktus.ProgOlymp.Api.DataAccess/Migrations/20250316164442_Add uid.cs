5823ccf7ba52ac49aca6fa1a39d16fbca8c2606e
Artem Krylov <kav128@outlook.com>
2025-03-16 19:47:01 +0300

    add migration

---
+++ Texnokaktus.ProgOlymp.Api.DataAccess/Migrations/20250316164442_Add uid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Texnokaktus.ProgOlymp.Api.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adduid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Uid",
+                table: "Applications",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uid",
+                table: "Applications");
+        }
+    }
+}
