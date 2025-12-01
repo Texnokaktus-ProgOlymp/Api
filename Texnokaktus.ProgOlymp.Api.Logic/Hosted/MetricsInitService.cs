c4be87900bf83c038e330f3ab1c9ee61ca189de3
Artem Krylov <kav128@outlook.com>
2025-12-01 22:20:51 +0300

    update MetricsInitService

--- Texnokaktus.ProgOlymp.Api.Logic/Hosted/MetricsInitService.cs
+++ Texnokaktus.ProgOlymp.Api.Logic/Hosted/MetricsInitService.cs
@@ -1,4 +1,3 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Texnokaktus.ProgOlymp.Api.DataAccess.Context;
@@ -6,9 +5,9 @@ using Texnokaktus.ProgOlymp.Api.Logic.Observability;
 
 namespace Texnokaktus.ProgOlymp.Api.Logic.Hosted;
 
-public class MetricsInitService(IServiceProvider serviceProvider) : BackgroundService
+public class MetricsInitService(IServiceProvider serviceProvider) : IHostedService
 {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
         await using var scope = serviceProvider.CreateAsyncScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -16,12 +15,14 @@ public class MetricsInitService(IServiceProvider serviceProvider) : BackgroundSe
         var authenticatedUsersCounter = MeterProvider.Meter.CreateAuthenticatedUsersCounter();
         var registeredUsersCounter = MeterProvider.Meter.CreateRegisteredUsersCounter();
 
-        await foreach (var user in context.Users.AsAsyncEnumerable().WithCancellation(stoppingToken))
+        await foreach (var user in context.Users.AsAsyncEnumerable().WithCancellation(cancellationToken))
             authenticatedUsersCounter.Add(1, KeyValuePair.Create<string, object?>("login", user.Login));
 
-        await foreach (var application in context.Applications.Where(application => application.ContestId == 1).AsAsyncEnumerable().WithCancellation(stoppingToken))
+        await foreach (var application in context.Applications.AsAsyncEnumerable().WithCancellation(cancellationToken))
             registeredUsersCounter.Add(1,
                                        KeyValuePair.Create<string, object?>("contestId", application.ContestId),
                                        KeyValuePair.Create<string, object?>("regionId", application.RegionId));
     }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }
