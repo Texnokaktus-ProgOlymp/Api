69ba8a0be0d623e2cd8fd761d9a019eb368dc444
Artem Krylov <kav128@outlook.com>
2026-03-28 20:59:46 +0300

    add migration

---
+++ db-migrations/52-53.sql
@@ -0,0 +1,16 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Contests]
+    ADD [PreliminaryStage_State] INT NULL;
+
+ALTER TABLE [Contests]
+    ADD [FinalStage_State] INT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260328175600_Add contest state', N'10.0.5');
+
+COMMIT;
+GO
+
+UPDATE [Contests]
+SET [PreliminaryStage_State] = 0,
+    [FinalStage_State] = 0
